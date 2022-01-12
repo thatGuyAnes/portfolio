1f33ec6d53e2429945ee8917a10993157cdde3e0
Anes <marzoukianas@gmail.com>
2022-01-12 11:30:07 +0100

    Add locomotive styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import RootLayout from './src/components/RootLayout';
 import './src/styles/normalize.css';
 import './src/styles/global.scss';
 import './src/styles/loader.scss';
+import './src/styles/locomotive-scroll.css';
 // import Cursor from './static/scripts/cursor';
 
 export const onInitialClientRender = () => {
