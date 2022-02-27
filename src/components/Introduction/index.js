edda66527540896f90ab98101e0e038ae6093ba4
Anes <marzoukianas@gmail.com>
2022-02-27 12:15:43 +0100

    seo & intro text

--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -86,7 +86,7 @@ const Intro = () => {
         const chars = Splitting({ target: line, by: 'chars' });
         textRevealAnimation.from(
           chars[0].chars,
-          { yPercent: 270, stagger: 0.03, duration: 0.2 },
+          { yPercent: 290, stagger: 0.03, duration: 0.2 },
           '>-80%'
         );
       });
